b0fcb8a961fcb8d2d0054f3cfb46a9f388dbe7c9
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-21 22:09:01 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,2 +1,27 @@
 <?php include_once("../layouts/header.php"); ?>
 <?php include_once("../layouts/nav_login.php"); ?>
+
+<div class="login-content">
+    <div class="login-content-title">
+        <i class='fas fa-user-alt' ></i> &nbsp;&nbsp;Login Here
+    </div>
+    <div class="login-content-body">
+        <div class="login-box">
+            <div class="login-box-header">
+            login
+            </div>
+            <div class="login-box-body">       
+                <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <div class="label">Username</div>
+                    <input type="text" name="username" id="username" class="login-input" placeholder="Username" required>
+
+                    <div class="label">Password</div>
+                    <input type="password" name="password" id="password" class="login-input" placeholder="Password" required>
+                    <br><br>
+                    <input type="submit" value="Login" class="login-btn" name="login"> 
+                </form> 
+            </div>
+        </div>           
+    </div>
+
+</div>
