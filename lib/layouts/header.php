0029f1420420b87ac30b33e992b8b6dab656f883
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-22 05:19:11 +0530

    Update header.php

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bill Alart System</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <title>e-Bill Alert System</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
