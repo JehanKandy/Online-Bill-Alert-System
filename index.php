2bb34b0a609b730de75071e364c586a08749d490
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-21 20:45:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php include_once("lib/layouts/header.php"); ?>
 <?php include_once("lib/layouts/nav.php");?>
+
+<div class="home">
+    <div class="home-content-title">
+        Welcome to Bill Alert System
+    </div>
+    <div class="home-content-body">
+        Join to get notification about your Bill
+    </div>
+    <div class="home-content-btn">
+        <a href="lib/views/login.php"><button class="content-btn">Join With Us..!</button></a>
+    </div>
+</div>
