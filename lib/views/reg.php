cc1c84aaa68cbb6bbae1de1772ee137ee3dff60c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-22 05:51:07 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -3,7 +3,7 @@
 
 <div class="reg-content">
     <div class="reg-content-title">
-        Register
+    <i class='fas fa-user-plus'></i>&nbsp;&nbsp;Register
     </div>
     <div class="reg-content-body">
         <div class="reg-box">
@@ -12,17 +12,20 @@
             </div>
             <div class="reg-box-body">
                 <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
-                    <div class="label">Username</div>
+                    <div class="reg-label">Username</div>
                     <input type="text" name="username" id="username" class="login-input" placeholder="Username">
 
-                    <div class="label">Email</div>
+                    <div class="reg-label">Email</div>
                     <input type="email" name="email" id="email" class="login-input" placeholder="Email">
             
-                    <div class="label">Password</div>
+                    <div class="reg-label">Password</div>
                     <input type="password" name="password" id="password" class="login-input" placeholder="Password">
 
-                    <div class="label">Confirm Password</div>
+                    <div class="reg-label">Confirm Password</div>
                     <input type="password" name="cpassword" id="cpassword" class="login-input" placeholder="Confirm Password">
+
+                    <input type="reset" value="Clear" class="reset-btn">&nbsp;
+                    <input type="submit" value="Register" name="register" class="reg-btn">
                 </form>
             </div>
         </div>
