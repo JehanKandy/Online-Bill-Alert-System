5cabf32ca3ce33fbf4be5b60e508da6eacf95059
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-24 21:04:54 +0530

    Add files via upload

---
+++ lib/layouts/nav_loged.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../../css/style.css">
+<?php include_once("../function/function.php"); ?>
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;University Campus - Dashboard</a>
+    <span class="navbar-text">
+        <i class='fas fa-user-alt' style='font-size:18px'></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="../views/logout.php" target="_top"><button class="logout-btn-nav"> Logout</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+</nav>
