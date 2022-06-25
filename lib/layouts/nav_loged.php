6114631ffbeea6c5c883407547f02344b3b56d28
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-25 20:50:14 +0530

    Update nav_loged.php

--- lib/layouts/nav_loged.php
+++ lib/layouts/nav_loged.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">&nbsp;&nbsp;University Campus - Dashboard</a>
     <span class="navbar-text">
-        <i class='fas fa-user-alt' style='font-size:18px'></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <i class='fas fa-user-alt' style='font-size:18px'></i>&nbsp;&nbsp;<?php user_id(); ?>&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="../views/logout.php" target="_top"><button class="logout-btn-nav"> Logout</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
     </span>
 </nav>
